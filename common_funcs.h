836e53ef2db878103f02fb8ba2f479b3fe9b5151
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2022-10-05 09:25:20 -0300

    Update common_funcs.h

--- common_funcs.h
+++ common_funcs.h
@@ -11,3 +11,27 @@ int lee_valida (int cota_min, int cota_max) {
   } while (value < cota_min || value > cota_max);
   return value;
 }
+
+void bubble_sort (int arr[], int num, int conf) {
+  int i, j, aux;
+  
+  // conf definira si el ordenamiento se lleva a cabo de manera ascendente o descendente
+  // si conf es igual a 1, el ordenamiento sera de manera creciente
+  // si conf es igual a 0, el ordenamiento sera de manera decreciente
+  for (i = 0; i < num; i++)
+    for (j = 0; j < num - 1 - i; j++) {
+      if (conf) {
+        if (arr[j] > arr[j + 1]) {
+          aux = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = aux;
+        }
+      } else {
+        if (arr[j] < arr[j + 1]) {
+          aux = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = aux;
+        }
+      }
+    }
+}
