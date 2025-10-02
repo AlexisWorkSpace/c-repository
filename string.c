37cf70ff0f670343758005f5bca33ba5ba20de0c
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2025-10-02 08:50:16 -0300

    Update string.c

--- string.c
+++ string.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-#include <string.h>
 
-void rep_dig(char *str, int pos) {
-    int len = strlen(cadena);
-    if (pos >= 0 && pos < len) str[pos] = '0';
-    else printf("fuera de rango\n");
+void main(void) {
+    uint8_t valor = 0xFF; // 0b11111111
+    uint8_t mascara;
+
+    for (uint8_t i = 0; i < 6; i++) {
+        mascara = ~(1 << i);     
+        valor = 0xFF & mascara;  
+        PORTB = valor;
+        __delay_ms(500); 
+    }
+
+    while (1);
 }
